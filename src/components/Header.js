27d0416046704458e7671dea50e63283b1c7e68f
Marine Lodynski <marine.lodynski1@gmail.com>
2024-06-12 17:31:35 +0200

    Ajout React router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import logo from '../images/logo_desktop.png';
 import '../styles/Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className='kasa-header'>
             <img src={logo} alt='Logo Kasa' className='kasa-logo' />
             <nav>
-                
+            <li><Link to="/Home">Accueil</Link></li>
+            <li><Link to="/About">A Propos</Link></li>
             </nav>
         </div>
     )
