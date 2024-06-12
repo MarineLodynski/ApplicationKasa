38265ceb5269283527b0d9058167cbb4c40cb37c
Marine Lodynski <marine.lodynski1@gmail.com>
2024-06-12 18:35:36 +0200

    Les routes fonctionnent bien et il faudra insérer des dossiers pour chaque page et composant

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './components/App';
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from '../src/pages/Home';
+import About from '../src/pages/About';
+import Logement from '../src/pages/Logement';
+import Error from '../src/pages/Error';
+ 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <Router>
+            <Routes>
+              <Route path="/Home" element={<Home />} />;
+              <Route path="/About" element={<About />} />;
+              <Route path="/Logement" element={<Logement />} />;
+              <Route path="*" element={<Error />} />;
+            </Routes>
+                  </Router>
+    </React.StrictMode>,
+document.getElementById('root')
+)
 
 
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import './styles/index.css';
+//import App from './components/App';
 
-
-
+//ReactDOM.render(
+  //<React.StrictMode>
+    //<App />
+  //</React.StrictMode>,
+  //document.getElementById('root')
+//);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
